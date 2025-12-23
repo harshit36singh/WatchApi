a36be3e8349b3e8e309156942e171395c693cd8f
harshit36singh <singh36harshit@gmail.com>
2025-12-23 22:41:53 +0530

    added frontend and backend

---
+++ Backend/watchapi/src/main/java/com/example/watchapi/WatchapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.watchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchapiApplication.class, args);
+	}
+
+}
---
+++ Backend/watchapi/src/test/java/com/example/watchapi/WatchapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.watchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
