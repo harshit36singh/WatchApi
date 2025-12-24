852809f45ce83a6509d8d6bc6f4e9d6cc97af5c2
harshit36singh <singh36harshit@gmail.com>
2025-12-24 14:32:04 +0530

    made the controller

---
+++ Backend/watchapi/src/main/java/com/example/watchapi/Controller/DashBoardController.java
@@ -0,0 +1,27 @@
+package com.example.watchapi.Controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.watchapi.Service.ActuatorDash;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/watch")
+@RequiredArgsConstructor
+public class DashBoardController {
+    
+    private final ActuatorDash actuatorDash;
+
+
+    @GetMapping("/dashboard")
+    public Mono<Map<String,Object>> dashbaord(@RequestParam String actautorurl ){
+        return actuatorDash.dashboard(actautorurl);
+    }
+}
